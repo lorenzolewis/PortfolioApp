e4a12ac1f9e311546a7947b8580d4007e1c233e3
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2020-10-30 17:51:38 -0500

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Lorenzo Lewis on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
