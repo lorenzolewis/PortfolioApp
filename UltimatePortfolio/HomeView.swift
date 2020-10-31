3dbd783cf23e6cda9c68258eeac41a073d0d6cef
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2020-10-30 22:36:34 -0500

    Initial commit.

---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Lorenzo Lewis on 10/30/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
